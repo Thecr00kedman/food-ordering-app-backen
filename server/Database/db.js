75832a4cfc4317ba72128728d16e41be905ae015
Aditya Sharma <117864728+Thecr00kedman@users.noreply.github.com>
2023-10-07 00:46:29 +0530

    fixed bugs

--- server/Database/db.js
+++ server/Database/db.js
@@ -3,14 +3,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const MONGO_URL=process.env.MONGO_URL;
 
-const Connection =async() => {
+
+const Connection =async(DB_USERNAME,DB_PASSWORD) => {
+    const MONGO_URL=`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@foodzone.qzj8k9g.mongodb.net/?retryWrites=true&w=majority`;
     try {
        await mongoose.connect(MONGO_URL,{useUnifiedTopology:true, useNewUrlParser:true});
         console.log('Database Connected Successfully')
         
     } catch (error) {
+        console.log(error)
         console.log("error while connecting to the database")
     }
 
--- server/index.js
+++ server/index.js
@@ -7,10 +7,14 @@ import bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express'
 import { options } from './config.js';
-
+import dotenv from 'dotenv'
 
 
 const app = express();
+
+dotenv.config()
+const DB_USERNAME=process.env.DB_USERNAME
+const DB_PASSWORD=process.env.DB_PASSWORD
 
 app.use(cors({
     origin: ["https://foodzone-b1d68.web.app/"],
@@ -36,6 +40,6 @@ const PORT= 8000;
 
 
 
-Connection();
+Connection(DB_USERNAME,DB_PASSWORD);
 
 app.listen(PORT,()=>console.log(`server is running at ${PORT}`))
